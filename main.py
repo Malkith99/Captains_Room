87ccf1e132851f192fc01f30d6c410019c10182a
Malkith99 <malkithamanda@gmail.com>
2021-06-15 12:20:06 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+k=int(input())
+list1=[int(item) for item in input().split()]
+list1=sorted(list1)
+for i in range(len(list1)):
+    if i!=len(list1)-1:
+        if list1[i]!=list1[i-1] and list1[i]!=list1[i+1]:
+            print(list1[i])
+            break
+    else:
+        print(list1[i])
